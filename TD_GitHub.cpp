1d93221b9ac53af082324961c94bddd73a78f24b
mateop42 <101389148+mateop42@users.noreply.github.com>
2022-03-18 08:45:55 +0100

    Ajout des fonctions estMajeur, exposant, TVA et plusGrand

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,13 +1,32 @@
 //Fonctions réalisées par l'étudiant 1 :
 
+bool estMajeur(int age)
+{
+	if (age >= 18)
+		return true;
+	else
+		return false;
+}
 
+int exposant(int nbre, int exp)
+{
+	return pow(nb, exp);
+}
 
+float TVA(int prix)
+{
+	return (prix + (prix*(19.6/100)));
+}
 
-
-
-
-
-
+int plusGrand(int a, int b, int c)
+{
+	if (a>b && a>c)
+		return a;
+	else if (b>a && b>c)
+		return b;
+	else
+		return c;
+}
 
 //Fonctions réalisées par l'étudiant 2 :
 
