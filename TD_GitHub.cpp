fea094e3e9e1b9d1abe808e02ac4846d638d286c
mateop42 <101389148+mateop42@users.noreply.github.com>
2022-03-18 09:02:25 +0100

    Ajout des fonctions estImpar, moyenne, reduction et nombreMedian

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -20,9 +20,9 @@ float TVA(int prix)
 
 int plusGrand(int a, int b, int c)
 {
-	if (a>b && a>c)
+	if (a>=b && a>=c)
 		return a;
-	else if (b>a && b>c)
+	else if (b>=a && b>=c)
 		return b;
 	else
 		return c;
@@ -40,3 +40,32 @@ int plusGrand(int a, int b, int c)
 
 
 //Fonctions réalisées par l'étudiant 3 :
+
+bool estImpair(int nb)
+{
+	if (nb%2 == 0)
+		return false;
+	else
+		return true;
+}
+
+float moyenne(int nb1, int nb2, int nb3)
+{
+	return ((nb1+nb2+nb3)/3);
+}
+
+float reduction(float prix, float reduc)
+{
+	return (prix*(prix/100));
+}
+
+int nombreMedian(int a, int b, int c)
+{
+	if (a>=b && a<=c)
+		return a;
+	else if (b>=a && b<=c)
+		return b;
+	else
+		return c;
+}
+
